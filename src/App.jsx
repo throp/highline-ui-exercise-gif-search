516d36557024b4cada2779f3d3d9435c10f1c852
Ben Northrop <hln-bnor@sheetz.com>
2023-06-27 18:22:09 -0400

    Commit for wireframe 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
+import React from 'react';
 import './App.css'
+import {searchGifs} from './api.js'
+
 
 function App() {
-  return (
-      <div>
-          Hello World
-      </div>
-  )
+    const [searchString, setSearchString] = React.useState()
+    const [images, setImages] = React.useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        searchGifs(searchString)
+            .then((data) => {
+                setImages(data.data);
+            });
+    }
+
+    const handleSearchStringChanged = (event) => {
+        setSearchString(event.target.value);
+    }
+
+    return (
+        <div>
+            <h1>Gif Search</h1>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Search" value={searchString} onChange={handleSearchStringChanged}/>
+                    <button>Search</button>
+                    <button>Clear</button>
+                </form>
+            </div>
+            <h2>Results</h2>
+            <div>
+                {
+                    images && images.map(image => (
+                        <div key={image.id} >
+                            <img src={image.images.original.url}></img>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+
 /*
 Create an account on https://developers.giphy.com/ and get an API key
 To get an api key go to the dashboard and click on "Create an App" and
@@ -6,7 +7,7 @@ then select "API" and then "Next Step" and then "Create App"
 copy the key and paste it in the API_KEY constant below
  */
 
-const API_KEY = "YOUR_API_KEY_HERE";
+const API_KEY = "DJNTd5U87XkO2f3i5de0ZJel6sSdd72w";
 
 export function searchGifs(searchTerm) {
     return fetch(
